9a8d4f8043448f3ab360d192c0593d09a4667258
JoshManalo <92727824+whiplashx@users.noreply.github.com>
2024-11-27 22:16:58 +0800

    feat: departments migration

--- database/migrations/2024_11_27_141314_areas.php
+++ database/migrations/2024_11_27_141314_areas.php
@@ -4,12 +4,12 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAreasTable extends Migration
+return new class extends Migration
 {
     public function up()
     {
         Schema::create('areas', function (Blueprint $table) {
-            $table->unsignedBigInteger('id', 10)->primary();
+            $table->string('id', 10)->primary();
             $table->string('name');
             $table->timestamps();
         });
@@ -19,5 +19,4 @@ class CreateAreasTable extends Migration
     {
         Schema::dropIfExists('areas');
     }
-}
-
+};
--- resources/js/Pages/Admin/Documents.jsx
+++ resources/js/Pages/Admin/Documents.jsx
@@ -1,10 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import AdminLayout from "@/Layouts/AdminLayout";
+import DataTable from "@/Components/DataTable";
 
 export default function departments(){
+
+    const handleSave = (formData) => {
+      console.log("User Data:", formData); // Handle the form data
+    };
+    const docData = [
+        {
+          id: 1,
+          name: 'Document A',
+          date: '2024-11-20',
+          uploader: 'Alice Johnson',
+          email: 'alice.johnson@example.com',
+        },
+        {
+          id: 2,
+          name: 'Report B',
+          date: '2024-11-22',
+          uploader: 'Bob Smith',
+          email: 'bob.smith@example.com',
+        },
+        {
+          id: 3,
+          name: 'Presentation C',
+          date: '2024-11-23',
+          uploader: 'Carol Williams',
+          email: 'carol.williams@example.com',
+        },
+        {
+          id: 4,
+          name: 'Spreadsheet D',
+          date: '2024-11-24',
+          uploader: 'David Brown',
+          email: 'david.brown@example.com',
+        },
+        {
+          id: 5,
+          name: 'Image E',
+          date: '2024-11-25',
+          uploader: 'Eve Davis',
+          email: 'eve.davis@example.com',
+        },
+      ];
+      
+  
+  
+    const [userData, setData] = useState([]);
+    axios.get('getUser')
+    .then(response=>{
+        setData(response.data);
+    })
+    .catch(error=>{
+        console.error('Error: ', error);
+    });
+    const columns = [
+        { key: 'id', label: 'ID' },
+        { key: 'department', label: 'Departments' },
+        { key: 'name', label: 'Name' },
+        { key: 'date', label: 'Date Added'},
+        { key: 'uploader', label: 'Uploader'},
+        { key: 'email', label: 'Email' },
+      ];
     return(
         <AdminLayout>
-            
+            <DataTable data={docData} columns={columns} itemsPerPage={10}/>
         </AdminLayout>
     );
 }
