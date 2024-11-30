1492f57358a1121dee50977950754d330840b004
JoshManalo <92727824+whiplashx@users.noreply.github.com>
2024-11-30 16:35:59 +0800

    feat: fixed email verification not sending email

--- routes/web.php
+++ routes/web.php
@@ -63,7 +63,6 @@ Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $requ
 })->middleware(['auth', 'signed'])->name('verification.verify'); 
 Route::post('/email/verification-notification', function (Request $request) {
     $request->user()->sendEmailVerificationNotification();
- 
     return back()->with('message', 'Verification link sent!');
 })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 require __DIR__.'/auth.php';
