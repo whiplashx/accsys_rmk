e4e32c846463c2546ee16368252a06bb0087e80a
ItsAlexxander <lwhip91@gmail.com>
2024-12-13 07:19:10 +0800

    put chat to right

--- resources/js/Components/AccreditationChatbot.jsx
+++ resources/js/Components/AccreditationChatbot.jsx
@@ -54,7 +54,7 @@ const AccreditationChatbot = () => {
   };
 
   return (
-    <div className="fixed bottom-4 left-4 z-50">
+    <div className="fixed bottom-4 right-4 z-50">
       {!isVisible && (
         <button
           onClick={toggleChat}
--- resources/js/Pages/LocalAccreditor/Selfsurvey.jsx
+++ resources/js/Pages/LocalAccreditor/Selfsurvey.jsx
@@ -6,18 +6,21 @@ import AccreditationView from "@/Components/AccreditationView";
 import LocalTaskForceTaskView from "@/Components/TasksComponent";
 import SelfSurveyForm from "@/Components/SelfSurveyForm";
 import AccreditationLayout from "@/Layouts/AccreditorLayout";
+import AccreditationChatbot from "@/Components/AccreditationChatbot";
+import AccreditorLayout_ from "@/Layouts/OutsideAccreditorLayout";
 
 export default function AccreditationLocalTaskForce(){
     return(
-        <AccreditationLayout>
+        <AccreditorLayout_>
                 <div className="min-h-screen bg-gray.-50">
 
       <main>
         <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
           <SelfSurveyForm></SelfSurveyForm>
+          <AccreditationChatbot></AccreditationChatbot>
         </div>
       </main>
       </div>
-        </AccreditationLayout>
+        </AccreditorLayout_>
     );
 }
